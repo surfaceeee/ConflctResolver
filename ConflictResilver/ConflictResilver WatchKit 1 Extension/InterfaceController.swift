4dc0ec65b7a27cbdd2015e8e69a6a379f97dc3dc
mc110 <yuzhe@mucang.cn>
2016-02-22 22:33:49 +0800

    初始化

---
+++ ConflictResilver/ConflictResilver WatchKit 1 Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  ConflictResilver WatchKit 1 Extension
+//
+//  Created by mc110 on 16/2/22.
+//  Copyright © 2016年 mc110. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
