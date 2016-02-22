10828fecbf5331d3730b8a6f25e35d8d9e73381d
mc110 <yuzhe@mucang.cn>
2016-02-22 23:32:56 +0800

    UIImageView动画

--- ConflictResilver/ConflictResilver/ViewController.swift
+++ ConflictResilver/ConflictResilver/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let i = UIImageView(frame: CGRectMake(0, 0, 40, 40))
+        let arr = [UIImage(named: "action0")!,UIImage(named: "action1")!,UIImage(named: "action2")!]
+        i.animationImages = arr
+        self.view.addSubview(i)
+        i.animationDuration = 3;
+        i.animationRepeatCount=2;
+        i.startAnimating()
     }
 
     override func didReceiveMemoryWarning() {
